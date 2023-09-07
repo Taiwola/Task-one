9ebd96ab06d698d152573f7a7b26ee0c6aa99bfe
Taiwola <seunolanitori@gmail.com>
2023-09-07 09:02:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "welcome"
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server Running on Port ${PORT}`)
+});
